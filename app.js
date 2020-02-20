1d72079750f719753cd16319ff00cce822be58ad
danielvebo90 <danielvebo@gmail.com>
2020-02-19 20:40:09 -0500

    1 repo

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get('/user/:name', (req, res) => {
-  let name = req.params.nombre;
-  if (!name || name.length === 0) {
+app.get('/makers/:nombre', (req, res) => {
+  let nombre = req.params.nombre;
+  if (!nombre || nombre.length === 0) {
     name = "desconocido";
   }
 
-  res.send(`<h1>Hola ${name}!</h1>`);
+  res.send(`<h1>Hola ${nombre}!</h1>`);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
